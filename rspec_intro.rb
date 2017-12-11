44d927e6515c58d70dd88bab7df0c10b553cec8f
NRothera <31472463+NRothera@users.noreply.github.com>
2017-12-11 15:40:02 +0000

    first commit

---
+++ rspec_intro.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+
+describe 'This is the description of the product we are going to test' do
+
+
+  context 'context is merely another way to break out your code and description' do
+
+    it 'the it block is where your tests begin and get run' do
+
+
+    end
+
+  end
+
+end
